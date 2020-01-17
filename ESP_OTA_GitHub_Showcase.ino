de811e75c9aea779e1792e12b5d0e76c043c1342
Gavin Smalley <gavin.smalley@googlemail.com>
2020-01-17 16:06:42 +0000

    Initial commit and v0.0.0 sketch.

---
+++ ESP_OTA_GitHub_Showcase.ino
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h>
+#include <FS.h>
+
+#ifndef STASSID
+#define STASSID "ssid"
+#define STAPSK  "password"
+#endif
+
+// A single, global CertStore which can be used by all
+// connections.  Needs to stay live the entire time any of
+// the WiFiClientBearSSLs are present.
+#include <CertStoreBearSSL.h>
+BearSSL::CertStore certStore;
+
+/* Set up values for your repository and binary names */
+#define GHOTA_USER "yknivag"
+#define GHOTA_REPO "ESP_OTA_GitHub_Showcase"
+#define GHOTA_CURRENT_TAG "0.0.0"
+#define GHOTA_BIN_FILE "ESP_OTA_GitHub_Showcase.ino.d1_mini.bin"
+#define GHOTA_ACCEPT_PRERELEASE 0
+
+#include <ESP_OTA_GitHub.h>
+// Initialise Update Code
+ESPOTAGitHub ESPOTAGitHub(&certStore, GHOTA_USER, GHOTA_REPO, GHOTA_CURRENT_TAG, GHOTA_BIN_FILE, GHOTA_ACCEPT_PRERELEASE);
+
+void setup() {
+	// Start serial for debugging (not used by library, just this sketch).
+	Serial.begin(115200);
+	
+	Serial.println();
+	Serial.println("================================================================================");
+	Serial.println("|                                                                              |");
+	Serial.println("|                Welcome to the ESP GitHub OTA Update Showcase                 |");
+	Serial.println("|                =============================================                 |");
+	Serial.println("|                                                                              |");
+	Serial.println("|    Authur:     Gavin Smalley                                                 |");
+	Serial.println("|    Repository: https://github.com/yknivag/ESP_OTA_GitHub_Showcase/           |");
+	Serial.println("|    Version:    0.0.0                                                         |");
+	Serial.println("|    Date:       17th January 2020                                             |");
+	Serial.println("|                                                                              |");
+	Serial.println("================================================================================");
+	Serial.println();
+	Serial.println();
+	
+	// Start SPIFFS and retrieve certificates.
+	SPIFFS.begin();
+	int numCerts = certStore.initCertStore(SPIFFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
+	Serial.print(F("Number of CA certs read: "));
+	Serial.println(numCerts);
+	if (numCerts == 0) {
+		Serial.println(F("No certs found. Did you run certs-from-mozill.py and upload the SPIFFS directory before running?"));
+		return; // Can't connect to anything w/o certs!
+	}
+
+	// Connect to WiFi
+	Serial.print("Connecting to WiFi... ");
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(STASSID, STAPSK);
+	if ((WiFi.status() != WL_CONNECTED)) {
+		Serial.print("... ");
+	}
+	Serial.println();
+  
+    /* This is the actual code to check and upgrade */
+    Serial.println("Checking for update...");
+    if (ESPOTAGitHub.checkUpgrade()) {
+		Serial.print("Upgrade found at: ");
+		Serial.println(ESPOTAGitHub.getUpgradeURL());
+		if (ESPOTAGitHub.doUpgrade()) {
+			Serial.println("Upgrade complete."); //This should never be seen as the device should restart on successful upgrade.
+		} else {
+			Serial.print("Unable to upgrade: ");
+			Serial.println(ESPOTAGitHub.getLastError());
+		}
+    } else {
+		Serial.print("Not proceeding to upgrade: ");
+		Serial.println(ESPOTAGitHub.getLastError());
+    }
+    /* End of check and upgrade code */
+  
+	// Your setup code goes here
+
+}
+
+void loop () {
+	// Your loop code goes here
+
+}
--- README.md
+++ README.md
@@ -6,17 +6,20 @@ This repository contains the sketches and binaries set-up with the correct relea
 
 ## Hardware
 
-The binaries attached are compiled for a Wemos D1 Mini
+The binaries attached are compiled for a Wemos D1 Mini with the memory map set to *"4MB (FS:1MB, OTA:~1019KB)"*.
 
 ## Instructions
 
-1. Download the binary from release 0.0.0 and flash it to your Wemos D1 Mini
-2. Attach the D1 Mini to a Serial Monitor
-3. Reset the module and confirm in the Serial Monitor that you are using version 0.0.0
-4. Use the captive portal to connect the device to your WiFi network.
-5. Keep watch in the Serial Monitor as the device updates.
-6. Verify on reboot that the device now shows 0.0.1 as the current version number.
+1. Download or clone the master branch.
+2. Add your WiFi credentials to the sketch.
+3. Ensure the memory map os set to *"4MB (FS:1MB, OTA:~1019KB)"* ("Tools" -> "Flash Size" in the Arduino IDE.
+4. Use "Tools" -> "ESP8266 Sketch Data Upload" to add the certificate file in the data directory of the sketch to your board.
+5. Attach the D1 Mini to a Serial Monitor at 115200 baud.
+6. Compile and upload the code.
+7. When the board resets you will see it print to the Serial monitor that it is version 0.0.0.
+8. Keep watch in the Serial Monitor as the device updates.
+9. Verify on reboot that the device now shows 0.1.0 as the current version number.
 
 ## Code
 
-The sketch provided here is essentally the same as the basic example for the [ESP_OTA_GitHub library](https://github.com/yknivag/ESP_OTA_GitHub) with the addition of the captive portal for connection.  You may use it as a starting point for your project but please do be sure to update the GitHub user, repository name, expected binary file name and version to match your own repository.
+The sketch provided here is essentally the same as the basic example for the [ESP_OTA_GitHub library](https://github.com/yknivag/ESP_OTA_GitHub).  You may use it as a starting point for your project but please do be sure to update the GitHub user, repository name, expected binary file name and version to match your own repository.
